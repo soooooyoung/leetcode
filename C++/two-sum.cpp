3d52dbbd712117162b55c3d0e3fde61510434d87
sooyoungkang <kang.sooyoung@hotmail.com>
2022-11-25 09:35:07 +0900

    Create two-sum.cpp

---
+++ C++/two-sum.cpp
@@ -0,0 +1,13 @@
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i = 0; i < nums.size(); i++) {
+            for(int j = 0; j < nums.size(); j++){
+                if(i == j){
+                    continue;
+                }
+                if(nums[i] + nums[j] == target){
+                    return {i,j};
+                }
+            }
+        }
+        return {0,0};
+    }
