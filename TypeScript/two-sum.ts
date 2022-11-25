f4bfabf8e607fe88dceaf34531ea31bcc96bb68e
sooyoungkang <kang.sooyoung@hotmail.com>
2022-11-25 09:49:45 +0900

    Create two-sum.ts

---
+++ TypeScript/two-sum.ts
@@ -0,0 +1,12 @@
+function twoSum(nums: number[], target: number) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i === j) {
+        continue;
+      }
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
