4fb60b28c24b9a01966634fa55492f05cb58372f
sooyoungkang <kang.sooyoung@hotmail.com>
2022-12-03 11:03:37 +0900

    Update median-of-two-sorted-arrays.ts

--- TypeScript/median-of-two-sorted-arrays.ts
+++ TypeScript/median-of-two-sorted-arrays.ts
@@ -1,11 +1,6 @@
 function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
   const arr = nums1.concat(nums2).sort((a, b) => a - b);
 
-  if (arr.length % 2 === 0) {
-    const x = Math.trunc(arr.length / 2);
-    const y = x - 1;
-    return (arr[x] + arr[y]) / 2;
-  } else {
-    return arr[Math.trunc(arr.length / 2)];
-  }
+  const x = Math.trunc(arr.length / 2);
+  return arr.length % 2 === 0 ? (arr[x] + arr[x - 1]) / 2 : arr[x];
 }
