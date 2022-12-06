688fa2a18ba022b3f09e710d0ea53ba436ee95fe
SooYoungKang <kang.sooyoung@hotmail.com>
2022-12-06 16:58:13 +0900

    Update reverse-integer.java

--- Java/reverse-integer.java
+++ Java/reverse-integer.java
@@ -6,12 +6,14 @@ class Solution {
 
         while (num != 0) {
             int current = x % 10;
-            if (result > Integer.MAX_VALUE / 10 || result == Integer.MAX_VALUE && current > 7 ||
-                    result < Integer.MIN_VALUE / 10 || result == Integer.MIN_VALUE && current < -8) {
+            if (result > Integer.MAX_VALUE / 10 ||
+                    (result == Integer.MAX_VALUE / 10 && current > 7) ||
+                    result < Integer.MIN_VALUE / 10 ||
+                    (result == Integer.MIN_VALUE / 10 && current < -8)) {
                 return 0;
             }
-            result = (result * 10) + num % 10;
-            num = num / 10;
+            result = (result * 10) + current;
+            num /= 10;
         }
 
         return result;
